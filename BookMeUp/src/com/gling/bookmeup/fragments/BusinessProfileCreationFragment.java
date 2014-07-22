7b6a42a31bb83b556598181239c9fec55a832fc6
galfalkon <galfalkon@hotmail.com>
2014-07-22 21:10:57 +0300

    Initial commit

---
+++ BookMeUp/src/com/gling/bookmeup/fragments/BusinessProfileCreationFragment.java
@@ -0,0 +1,17 @@
+package com.gling.bookmeup.fragments;
+
+import android.view.View;
+
+import com.gling.bookmeup.R;
+import com.gling.bookmeup.main.OnClickListenerFragment;
+
+public class BusinessProfileCreationFragment extends OnClickListenerFragment {
+	@Override
+	protected int getFragmentLayoutId() {
+		return R.layout.fragment_business_profile_creation;
+	}
+
+	@Override
+	public void onClick(View v) {
+	}
+}
