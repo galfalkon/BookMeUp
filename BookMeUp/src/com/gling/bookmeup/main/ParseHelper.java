6fa17c020557d58de3c0f95cf0e3bdaf051a5831
galfalkon <galfalkon@hotmail.com>
2014-07-25 14:51:17 +0300

    Adding Clients and Bookings classes to ParseHelper

--- BookMeUp/src/com/gling/bookmeup/main/ParseHelper.java
+++ BookMeUp/src/com/gling/bookmeup/main/ParseHelper.java
@@ -2,6 +2,15 @@ package com.gling.bookmeup.main;
 
 public class ParseHelper {
 	
+	public static class ClientsClass {
+		public final static String CLASS_NAME = "Clients";
+		
+		public static class Keys {
+			public static final String ID = "objectId";
+			public static final String FAVOURITES = "favourites";
+		}
+	}
+	
 	public static class BusinessesClass {
 		public final static String CLASS_NAME = "Businesses";
 		
@@ -14,6 +23,18 @@ public class ParseHelper {
 			public static final String PICTURE = "picture";
 			public static final String SERVICES = "services";
 			public static final String OFFERS = "offers";
+		}
+	}
+	
+	public static class BookingsClass {
+		public final static String CLASS_NAME = "Bookings";
+		
+		public static class Keys {
+			public static final String CLIENT_ID = "clientId";
+			public static final String BUSINESS_ID = "businessId";
+			public static final String DATE = "date";
+			public static final String SERVICES = "services";
+			public static final String IS_APPROVED = "isApproved";
 		}
 	}
 }
