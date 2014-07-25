fb1890e0a87cead12a1424923ea0fc4a5c7ee687
galfalkon <galfalkon@hotmail.com>
2014-07-25 14:21:14 +0300

    ParseHelper
    ParseHelper currently contains information about the Businesses class.

---
+++ BookMeUp/src/com/gling/bookmeup/main/ParseHelper.java
@@ -0,0 +1,19 @@
+package com.gling.bookmeup.main;
+
+public class ParseHelper {
+	
+	public static class BusinessesClass {
+		public final static String CLASS_NAME = "Businesses";
+		
+		public static class Keys {
+			public static final String ID = "objectId";
+			public static final String DISPLAY_NAME = "displayName";
+			public static final String DESCRIPTION = "description";
+			public static final String CATEGORY = "category";
+			public static final String OPENING_HOURS = "openingHours";
+			public static final String PICTURE = "picture";
+			public static final String SERVICES = "services";
+			public static final String OFFERS = "offers";
+		}
+	}
+}
