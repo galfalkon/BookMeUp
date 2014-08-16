d56ee0005ac07f8b26753ba6dfe29ca53fa557eb
lev-kazakov <lev.kazakov@gmail.com>
2014-08-16 15:53:18 +0300

    Business calendar - empty fragment

---
+++ BookMeUp/src/com/gling/bookmeup/business/fragments/BusinessCalendarFragment.java
@@ -0,0 +1,23 @@
+package com.gling.bookmeup.business.fragments;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
+
+import com.gling.bookmeup.R;
+import com.gling.bookmeup.main.OnClickListenerFragment;
+
+public class BusinessCalendarFragment extends OnClickListenerFragment implements OnClickListener {
+
+	private static final String TAG = "BusinessCalendarFragment";
+
+	@Override
+	protected int getFragmentLayoutId() {
+		return R.layout.business_calendar_fragment;
+	}
+
+	@Override
+	public void onClick(View v) {
+		Toast.makeText(getActivity(), "Not implemented", Toast.LENGTH_SHORT).show();
+	}
+}
