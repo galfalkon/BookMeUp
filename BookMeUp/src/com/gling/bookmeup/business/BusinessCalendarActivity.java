b461c416660f635f5d63c1440c78b892e587b7d4
lev-kazakov <lev.kazakov@gmail.com>
2014-09-07 17:49:48 +0300

    minor

--- BookMeUp/src/com/gling/bookmeup/business/BusinessCalendarActivity.java
+++ BookMeUp/src/com/gling/bookmeup/business/BusinessCalendarActivity.java
@@ -3,16 +3,6 @@ package com.gling.bookmeup.business;
 import org.joda.time.DateTime;
 
 import com.gling.bookmeup.R;
-import com.gling.bookmeup.R.id;
-import com.gling.bookmeup.R.layout;
-import com.gling.bookmeup.R.menu;
-import com.gling.bookmeup.R.string;
-import com.google.gson.Gson;
-import com.parse.ParseInstallation;
-
-import android.app.Activity;
-import android.app.ActionBar;
-import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -20,7 +10,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -34,7 +23,7 @@ public class BusinessCalendarActivity extends FragmentActivity {
     
     private static final int DAYS_MARGIN = 30;
     
-    SectionsPagerAdapter mSectionsPagerAdapter;
+    DayViewPagerAdapter mSectionsPagerAdapter;
     ViewPager mViewPager;
 
     @Override
@@ -45,29 +34,27 @@ public class BusinessCalendarActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.business_calendar_activity);
 
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        // this adapter will generate a new BusinessCalendarDayViewFragment for each day.
+        // it will allow scrolling for up to DAYS_MARGIN days before current time,
+        // and up to DAYS_MARGIN days after current time
+        mSectionsPagerAdapter = new DayViewPagerAdapter(getSupportFragmentManager());
 
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.business_calendar_activity_pager);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
+        // set pager to current date
         mViewPager.setCurrentItem(DAYS_MARGIN);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.business_calendar, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.business_calendar_action_today) {
             mViewPager.setCurrentItem(DAYS_MARGIN);
@@ -76,17 +63,15 @@ public class BusinessCalendarActivity extends FragmentActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public class SectionsPagerAdapter extends FragmentStatePagerAdapter {
+    public class DayViewPagerAdapter extends FragmentStatePagerAdapter {
 
-        private DateTime dt = new DateTime();
-        
-        public SectionsPagerAdapter(FragmentManager fm) {
+        public DayViewPagerAdapter(FragmentManager fm) {
             super(fm);
         }
 
         @Override
         public Fragment getItem(int position) {
-            return BusinessCalendarFragment.newInstance(position);
+            return BusinessCalendarDayViewFragment.newInstance(DateTime.now().minusDays(DAYS_MARGIN).plusDays(position));
         }
 
         @Override
@@ -95,31 +80,32 @@ public class BusinessCalendarActivity extends FragmentActivity {
         }
     }
 
-    public static class BusinessCalendarFragment extends Fragment {
+    public static class BusinessCalendarDayViewFragment extends Fragment {
         /**
          * The fragment argument representing the section number for this
          * fragment.
          */
-        private static final String ARG_SECTION_NUMBER = "section_number";
+        private static final String ARG_DATE = "section_date";
 
         /**
-         * Returns a new instance of this fragment for the given section number.
+         * Returns a new instance of this fragment for the given section date.
          */
-        public static BusinessCalendarFragment newInstance(int sectionNumber) {
-            BusinessCalendarFragment fragment = new BusinessCalendarFragment();
+        public static BusinessCalendarDayViewFragment newInstance(DateTime dateTime) {
+            BusinessCalendarDayViewFragment fragment = new BusinessCalendarDayViewFragment();
             Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            args.putSerializable(ARG_DATE, dateTime);
             fragment.setArguments(args);
             return fragment;
         }
-
-        public BusinessCalendarFragment() {
+        
+        public BusinessCalendarDayViewFragment() {
+            
         }
-
+        
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.business_calendar_fragment, container, false);
-            ((TextView)rootView.findViewById(R.id.section_label)).setText(String.valueOf(getArguments().getInt(ARG_SECTION_NUMBER)));
+            ((TextView)rootView.findViewById(R.id.section_label)).setText(getArguments().getSerializable(ARG_DATE).toString());
             return rootView;
         }
     }
