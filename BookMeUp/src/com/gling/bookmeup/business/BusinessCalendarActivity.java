15bdff5800fcad4013a44b3437075b4bf46775a7
lev-kazakov <lev.kazakov@gmail.com>
2014-09-11 16:51:08 +0300

    Business Calendar - can go-to date

--- BookMeUp/src/com/gling/bookmeup/business/BusinessCalendarActivity.java
+++ BookMeUp/src/com/gling/bookmeup/business/BusinessCalendarActivity.java
@@ -1,9 +1,9 @@
 package com.gling.bookmeup.business;
 
 import org.joda.time.DateTime;
-import org.joda.time.DateTimeField;
 
-import com.gling.bookmeup.R;
+import android.app.DatePickerDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -11,12 +11,11 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.DatePicker;
+
+import com.gling.bookmeup.R;
 
 public class BusinessCalendarActivity extends FragmentActivity {
 
@@ -24,8 +23,9 @@ public class BusinessCalendarActivity extends FragmentActivity {
     
     private static final int DAYS_MARGIN = 30;
     
-    CalendarPagerAdapter mSectionsPagerAdapter;
-    ViewPager mViewPager;
+    DateTime _today;
+    CalendarPagerAdapter _sectionsPagerAdapter;
+    ViewPager _viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,17 +35,20 @@ public class BusinessCalendarActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.business_calendar_activity);
 
-        // this adapter will generate a new BusinessCalendarDayViewFragment for each day.
-        // it will allow scrolling for up to DAYS_MARGIN days before current time,
-        // and up to DAYS_MARGIN days after current time
-        mSectionsPagerAdapter = new CalendarPagerAdapter(getSupportFragmentManager());
+        DateTime now = new DateTime();
+        _today = new DateTime(now.getYear(), now.getMonthOfYear(), now.getDayOfMonth(), 0, 0);
+        
+        // this adapter will generate a new BusinessDayViewFragment for each day.
+        // it will allow scrolling for up to DAYS_MARGIN days before the anchor date,
+        // and up to DAYS_MARGIN days after the anchor date
+        _sectionsPagerAdapter = new CalendarPagerAdapter(getSupportFragmentManager(), _today);
 
         // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.business_calendar_activity_pager);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
+        _viewPager = (ViewPager) findViewById(R.id.business_calendar_activity_pager);
+        _viewPager.setAdapter(_sectionsPagerAdapter);
 
         // set pager to current date
-        mViewPager.setCurrentItem(DAYS_MARGIN);
+        _viewPager.setCurrentItem(DAYS_MARGIN);
     }
 
     @Override
@@ -56,25 +59,54 @@ public class BusinessCalendarActivity extends FragmentActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.business_calendar_action_today) {
-            mViewPager.setCurrentItem(DAYS_MARGIN);
+        switch (item.getItemId()) {
+        case R.id.business_calendar_action_today:
+            if (!_sectionsPagerAdapter.getAnchor().equals(_today)) {
+                _sectionsPagerAdapter = new CalendarPagerAdapter(getSupportFragmentManager(), _today);
+                _viewPager.setAdapter(_sectionsPagerAdapter);
+            }
+            _viewPager.setCurrentItem(DAYS_MARGIN);
             return true;
+        case R.id.business_calendar_action_pick:
+            handleDatePicker();
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
+    }
+    
+    private void handleDatePicker() {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                DateTime selectedDate = new DateTime(year, monthOfYear + 1, dayOfMonth, 0, 0);
+                Log.i(TAG, "Date: " + selectedDate + " selected");
+                if (!_sectionsPagerAdapter.getAnchor().equals(selectedDate)) {
+                    _sectionsPagerAdapter = new CalendarPagerAdapter(getSupportFragmentManager(), selectedDate);
+                    _viewPager.setAdapter(_sectionsPagerAdapter);
+                }
+                _viewPager.setCurrentItem(DAYS_MARGIN);
+            }
+        }, _today.getYear(), _today.getMonthOfYear() - 1, _today.getDayOfMonth());
+        datePickerDialog.show();
     }
 
     public class CalendarPagerAdapter extends FragmentStatePagerAdapter {
 
-        public CalendarPagerAdapter(FragmentManager fm) {
+        private DateTime _anchorDate;
+        
+        public CalendarPagerAdapter(FragmentManager fm, DateTime anchorDate) {
             super(fm);
+            _anchorDate = anchorDate;
+        }
+        
+        public DateTime getAnchor() {
+            return _anchorDate;
         }
 
         @Override
         public Fragment getItem(int position) {
-            DateTime now = DateTime.now();
-            DateTime today = new DateTime(now.getYear(), now.getMonthOfYear(), now.getDayOfMonth(), 0, 0);
-            return BusinessCalendarFragment.newInstance(today.minusDays(DAYS_MARGIN).plusDays(position));
+            return BusinessCalendarFragment.newInstance(_anchorDate.minusDays(DAYS_MARGIN).plusDays(position));
         }
 
         @Override
