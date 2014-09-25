8d88e99e368034ca9e90c8e33e458e8db110dc84
galfalkon <galfalkon@hotmail.com>
2014-09-25 22:07:58 +0300

    Delete comments

--- BookMeUp/src/com/gling/bookmeup/main/NavigationDrawerActivity.java
+++ BookMeUp/src/com/gling/bookmeup/main/NavigationDrawerActivity.java
@@ -6,11 +6,7 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.gling.bookmeup.R;
 
