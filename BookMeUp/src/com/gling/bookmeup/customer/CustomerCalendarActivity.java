c7ad9ddea5dc0fb3af1945fde1b3c245e2ec0cf1
gefenHuji <gefen.naar@mail.huji.ac.il>
2014-10-11 00:11:27 +0300

    Adding calendar activity

---
+++ BookMeUp/src/com/gling/bookmeup/customer/CustomerCalendarActivity.java
@@ -0,0 +1,187 @@
+package com.gling.bookmeup.customer;
+
+import org.joda.time.DateTime;
+
+import android.app.DatePickerDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewTreeObserver;
+import android.view.ViewTreeObserver.OnGlobalLayoutListener;
+import android.widget.DatePicker;
+
+import com.astuetz.PagerSlidingTabStrip;
+import com.gling.bookmeup.R;
+
+public class CustomerCalendarActivity extends FragmentActivity {
+
+	private static final String TAG = "CustomerCalendarActivity";
+
+	private static final int DAYS_MARGIN = 30;
+
+	private DateTime _today;
+
+	private PagerSlidingTabStrip _tabs;
+	private ViewPager _viewPager;
+	private CalendarPagerAdapter _sectionsPagerAdapter;
+	private int _scrollCenter;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+
+		Log.i(TAG, "onCreate");
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.business_calendar_activity);
+
+		DateTime now = new DateTime();
+		_today = new DateTime(now.getYear(), now.getMonthOfYear(),
+				now.getDayOfMonth(), 0, 0);
+
+		_tabs = (PagerSlidingTabStrip) findViewById(R.id.business_calendar_activity_tabs);
+		// Set up the ViewPager with the sections adapter.
+		_viewPager = (ViewPager) findViewById(R.id.business_calendar_activity_pager);
+		// this adapter will generate a new BusinessDayViewFragment for each
+		// day.
+		// it will allow scrolling for up to DAYS_MARGIN days before the anchor
+		// date,
+		// and up to DAYS_MARGIN days after the anchor date
+		_sectionsPagerAdapter = new CalendarPagerAdapter(
+				getSupportFragmentManager(), _today);
+		_viewPager.setAdapter(_sectionsPagerAdapter);
+		_tabs.setViewPager(_viewPager);
+
+		// set pager to current date
+		_viewPager.setCurrentItem(DAYS_MARGIN);
+		
+		// get the scroll offset of the central (today) tab
+		_scrollCenter = 0;
+		ViewTreeObserver vto = _tabs.getViewTreeObserver(); 
+		vto.addOnGlobalLayoutListener(new OnGlobalLayoutListener() {
+		    @SuppressWarnings("deprecation")
+			@Override
+		    public void onGlobalLayout() {
+		    	_scrollCenter = _tabs.getScrollX();
+		        ViewTreeObserver obs = _tabs.getViewTreeObserver();
+		        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+		            obs.removeOnGlobalLayoutListener(this);
+		        } else {
+		            obs.removeGlobalOnLayoutListener(this);
+		        }
+		    }
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.business_calendar, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.business_calendar_action_today:
+			if (!_sectionsPagerAdapter.getAnchor().equals(_today)) {
+				_sectionsPagerAdapter = new CalendarPagerAdapter(
+						getSupportFragmentManager(), _today);
+				_viewPager.setAdapter(_sectionsPagerAdapter);
+				_tabs.setViewPager(_viewPager);
+				_viewPager.setCurrentItem(DAYS_MARGIN, true);
+			} else {
+				_tabs.smoothScrollTo(_scrollCenter, 0);				
+			}
+			return true;
+		case R.id.business_calendar_action_pick:
+			handleDatePicker();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	private void handleDatePicker() {
+		DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+				new OnDateSetListener() {
+					@Override
+					public void onDateSet(DatePicker view, int year,
+							int monthOfYear, int dayOfMonth) {
+						DateTime selectedDate = new DateTime(year,
+								monthOfYear + 1, dayOfMonth, 0, 0);
+						Log.i(TAG, "Date: " + selectedDate + " selected");
+						if (!_sectionsPagerAdapter.getAnchor().equals(
+								selectedDate)) {
+							_sectionsPagerAdapter = new CalendarPagerAdapter(
+									getSupportFragmentManager(), selectedDate);
+							_viewPager.setAdapter(_sectionsPagerAdapter);
+							_tabs.setViewPager(_viewPager);
+							_viewPager.setCurrentItem(DAYS_MARGIN, true);
+						} else {
+							_tabs.smoothScrollTo(_scrollCenter, 0);				
+						}
+					}
+				}, _today.getYear(), _today.getMonthOfYear() - 1,
+				_today.getDayOfMonth());
+		datePickerDialog.show();
+	}
+
+	public class CalendarPagerAdapter extends FragmentStatePagerAdapter {
+
+		private DateTime _anchorDate;
+		private final static String DATE_FORMAT = "E, dd MMM";
+
+		private final static int COUNT = DAYS_MARGIN * 2 + 1;
+		private String[] titles = new String[COUNT];
+
+		public CalendarPagerAdapter(FragmentManager fm, DateTime anchorDate) {
+			super(fm);
+			_anchorDate = anchorDate;
+			generateTitles();
+		}
+
+		private void generateTitles() {
+
+			titles[DAYS_MARGIN] = _anchorDate.toString(DATE_FORMAT);
+			DateTime past = new DateTime(_anchorDate);
+			DateTime future = new DateTime(_anchorDate);
+
+			for (int i = 1; i <= DAYS_MARGIN; i++) {
+				past = past.plusDays(-1);
+				titles[DAYS_MARGIN - i] = past.toString(DATE_FORMAT);
+
+				future = future.plusDays(1);
+				titles[DAYS_MARGIN + i] = future.toString(DATE_FORMAT);
+			}
+
+		}
+
+		public DateTime getAnchor() {
+			return _anchorDate;
+		}
+
+		@Override
+		public CharSequence getPageTitle(int position) {
+			return titles[position];
+		}
+
+		@Override
+		public Fragment getItem(int position) {
+			return CustomerCalendarFragment.newInstance(_anchorDate.minusDays(
+					DAYS_MARGIN).plusDays(position));
+		}
+
+		@Override
+		public int getCount() {
+			return COUNT;
+		}
+	}
+
+}
---
+++ BookMeUp/src/com/gling/bookmeup/customer/CustomerCalendarFragment.java
@@ -0,0 +1,140 @@
+package com.gling.bookmeup.customer;
+
+import java.util.List;
+
+import org.joda.time.DateTime;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.gling.bookmeup.R;
+import com.gling.bookmeup.main.ParseHelper;
+import com.gling.bookmeup.main.ParseHelper.Booking;
+import com.parse.ParseQuery;
+import com.parse.ParseQueryAdapter;
+import com.parse.ParseQueryAdapter.OnQueryLoadListener;
+import com.parse.ParseUser;
+
+public class CustomerCalendarFragment extends Fragment {
+
+    private static final String TAG = "CustomerCalendarFragment";
+    private static final String ARG_DATE = "business_calendar_fragment_date";
+
+    private DateTime _date;
+    private CustomParseQueryAdapter _businessBookingsAdapter;
+    
+    private ListView _lstBookings;
+    private ProgressBar _progressBar;
+    
+    /**
+     * Returns a new instance of this fragment for the given date.
+     */
+    public static CustomerCalendarFragment newInstance(DateTime dateTime) {
+        CustomerCalendarFragment fragment = new CustomerCalendarFragment();
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_DATE, dateTime);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public CustomerCalendarFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "onCreate");
+        super.onCreate(savedInstanceState);
+        
+        _date = (DateTime) getArguments().getSerializable(ARG_DATE);
+    }
+    
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.business_calendar_fragment, container, false);
+        
+        _lstBookings = (ListView) rootView.findViewById(R.id.business_calendar_list);
+        _progressBar = (ProgressBar) rootView.findViewById(R.id.business_calendar_progress_bar);        
+        
+        _businessBookingsAdapter = new CustomParseQueryAdapter(getActivity());      
+        _lstBookings.setAdapter(_businessBookingsAdapter);
+        
+        _businessBookingsAdapter.addOnQueryLoadListener(new OnQueryLoadListener<Booking>() {
+            @Override
+            public void onLoading() {
+            	_lstBookings.setVisibility(View.VISIBLE);
+            	_progressBar.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onLoaded(List<Booking> objects, Exception e) {
+            	_progressBar.setVisibility(View.GONE);
+            	_lstBookings.setVisibility(View.VISIBLE);
+            }
+
+          });
+
+        return rootView;
+    }
+    
+    public class CustomParseQueryAdapter extends ParseQueryAdapter<Booking> {
+        
+        public CustomParseQueryAdapter(Context context) {
+            super(context, new ParseQueryAdapter.QueryFactory<Booking>() {
+                public ParseQuery<Booking> create() {
+                    ParseQuery<Booking> query = new ParseQuery<Booking>(Booking.CLASS_NAME);
+                    query.whereEqualTo(Booking.Keys.CUSTOMER_POINTER, ParseUser.getCurrentUser().get(ParseHelper.User.Keys.CUSTOMER_POINTER));
+                    query.whereGreaterThanOrEqualTo(Booking.Keys.DATE, _date.toDate());
+                    query.whereLessThan(Booking.Keys.DATE, _date.plusDays(1).toDate());
+                    query.include(Booking.Keys.BUSINESS_POINTER);
+                    query.include(Booking.Keys.SERVICE_POINTER);
+                    return query;
+                }
+            });
+        }
+
+        @Override
+        public View getItemView(Booking booking, View v, ViewGroup parent) {
+            if (v == null) {
+                v = View.inflate(getContext(), R.layout.business_calendar_item, null);
+            }
+            super.getItemView(booking, v, parent);
+            
+            TextView txtCustomerName = (TextView) v.findViewById(R.id.business_calendar_customer_name);
+//            ParseImageView imgCustomerImage = (ParseImageView) v.findViewById(R.id.business_calendar_customer_image);
+            TextView txtBookingTime = (TextView) v.findViewById(R.id.business_calendar_booking_date);
+            TextView txtBookingService = (TextView) v.findViewById(R.id.business_calendar_booking_service);
+            TextView txtBookingStatus = (TextView) v.findViewById(R.id.business_calendar_booking_status);
+
+            // Add customer name
+            txtCustomerName.setText(booking.getCustomerName());
+            
+            // Add and download customer image
+//            ParseFile imageFile = booking.getParseFile("image");
+//            if (imageFile != null) {
+//                customerImage.setParseFile(imageFile);
+//                customerImage.loadInBackground();
+//            }
+
+            // Add booking time
+            txtBookingTime.setText(new DateTime(booking.getDate()).toString("HH:mm"));
+            
+            // Add booked service
+            txtBookingService.setText(booking.getServiceName());
+            
+            // Add booking status
+            txtBookingStatus.setText(String.valueOf(booking.getStatus()));
+            
+            return v;
+        }
+    }
+
+}
