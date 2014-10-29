3027639e673aaaae42472b459e7f75f54d5954bb
galfalkon <galfalkon@hotmail.com>
2014-10-29 22:37:23 +0200

    BusinessEmaillLogin refactor

--- BookMeUpForBusiness/src/com/gling/bookmeup/business/login/BusinessEMailLoginFragment.java
+++ BookMeUpForBusiness/src/com/gling/bookmeup/business/login/BusinessEMailLoginFragment.java
@@ -8,10 +8,8 @@ import com.gling.bookmeup.business.BusinessMainActivity;
 import com.gling.bookmeup.business.wizards.profile.BusinessProfileWizardActivity;
 import com.gling.bookmeup.sharedlib.login.EMailLoginFragmentBase;
 import com.gling.bookmeup.sharedlib.parse.Business;
-import com.gling.bookmeup.sharedlib.parse.ParseHelper;
 import com.gling.bookmeup.sharedlib.parse.ParseHelper.Category;
 import com.gling.bookmeup.sharedlib.parse.ParseHelper.User;
-import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
@@ -26,62 +24,49 @@ public class BusinessEMailLoginFragment extends EMailLoginFragmentBase
 	@Override
 	protected void handleSuccessfulLogin(ParseUser user)
 	{
+		if (!user.getBoolean("emailVerified"))
+		{
+			Log.i(TAG, "User hasn't verified Email address");
+			Crouton.showText(getActivity(), "Please verifiy your Email address", Style.ALERT);
+			return;
+		}
+		
 		try
 		{
 			// Fetch current business
 			Log.i(TAG, "Fetching current business");
 			ParseObject businessParseObject = user.getParseObject(User.Keys.BUSINESS_POINTER);
-			if (businessParseObject != null)
+			if (businessParseObject == null)
 			{
-				Business currentBusiness = businessParseObject.fetchIfNeeded();
-				Category category = currentBusiness.getCategory();
-				if (category != null)
-				{
-					category.fetchIfNeeded();
-				}
-				Business.setCurrentBusiness(currentBusiness);
+				// TODO: Create Business
+				Crouton.showText(getActivity(), "TODO: Create Business (Current user isn't associated with a Business instance)", Style.INFO);
+				return;
 			}
+			
+			Business currentBusiness = businessParseObject.fetchIfNeeded();
+			Category category = currentBusiness.getCategory();
+			if (category != null)
+			{
+				category.fetchIfNeeded();
+			}
+			Business.setCurrentBusiness(currentBusiness);
+			
+			Intent intent;
+			if (TextUtils.isEmpty(currentBusiness.getName()))
+			{
+				intent = new Intent(getActivity(), BusinessProfileWizardActivity.class);
+			}
+			else
+			{
+				intent = new Intent(getActivity(), BusinessMainActivity.class);
+			}
+			startActivity(intent);
 		}
 		catch (ParseException e)
 		{
 			Log.i(TAG, "Exception: " + e.getMessage());
 			Crouton.showText(getActivity(), "Login failed: " + e.getMessage(), Style.ALERT);
 			return;
-		}
-
-		if (!user.getBoolean("emailVerified"))
-		{
-			Log.i(TAG, "User hasn't verified Email address");
-			Crouton.showText(getActivity(), "Please verifiy your Email address", Style.ALERT);
-		}
-		else if (user.getParseObject(User.Keys.BUSINESS_POINTER) != null)
-		{
-			ParseHelper.fetchBusiness(new GetCallback<Business>()
-			{
-
-				@Override
-				public void done(Business business, ParseException e)
-				{
-					if (e == null)
-					{
-						Intent intent;
-						if (TextUtils.isEmpty(business.getName()))
-						{
-							intent = new Intent(getActivity(), BusinessProfileWizardActivity.class);
-						}
-						else
-						{
-							intent = new Intent(getActivity(), BusinessMainActivity.class);
-						}
-						startActivity(intent);
-					}
-					else
-					{
-						Crouton.showText(getActivity(), "Oops, we're having difficulties, please try again...", Style.ALERT);
-						Log.i(TAG, "Business fetch failed: " + e.getMessage());
-					}
-				}
-			});
 		}
 	}
 }
