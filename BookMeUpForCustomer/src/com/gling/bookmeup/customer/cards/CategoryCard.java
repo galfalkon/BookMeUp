f02702cd7af717397a4b6d03c4733304cc0fc1d7
lev-kazakov <lev.kazakov@gmail.com>
2014-10-31 12:59:15 +0200

    UI - customer category card - add some icons

--- BookMeUpForCustomer/src/com/gling/bookmeup/customer/cards/CategoryCard.java
+++ BookMeUpForCustomer/src/com/gling/bookmeup/customer/cards/CategoryCard.java
@@ -4,6 +4,7 @@ import it.gmariotti.cardslib.library.internal.Card;
 import it.gmariotti.cardslib.library.internal.CardHeader;
 import it.gmariotti.cardslib.library.internal.CardThumbnail;
 import android.content.Context;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,14 +15,15 @@ import com.gling.bookmeup.sharedlib.parse.Business;
 import com.gling.bookmeup.sharedlib.parse.ParseHelper.Category;
 import com.parse.CountCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseQuery;
 
 public class CategoryCard extends Card {
 
     public static final String TAG = "CategoryCard";
-    
+
     private Category _category;
-    
+
     public CategoryCard(Context context, Category category) {
         super(context, R.layout.customer_category_card_inner_layout);
         _category = category;
@@ -31,36 +33,68 @@ public class CategoryCard extends Card {
         header.setTitle(_category.getName());
         addCardHeader(header);
 
-        CardThumbnail thumb = new CardThumbnail(getContext());
-        thumb.setDrawableResource(R.drawable.ic_launcher);
+        CardThumbnail thumb = new CategoryCardThumbnail(getContext());
+        ParseFile categoryImage = _category.getImageFile();
+        if (categoryImage != null) {
+            thumb.setUrlResource(categoryImage.getUrl());
+            thumb.setErrorResource(R.drawable.ic_launcher);
+        } else {
+            thumb.setDrawableResource(R.drawable.ic_launcher);
+            thumb.setExternalUsage(false);
+        }
         addCardThumbnail(thumb);
     }
 
     @Override
     public void setupInnerViewElements(ViewGroup parent, final View view) {
+        final TextView numBusinessesTxt = (TextView) view
+                                                         .findViewById(R.id.customer_category_card_num_businesses_txt);
+        numBusinessesTxt.setText("");
+
         ParseQuery<Business> query = new ParseQuery<Business>(Business.CLASS_NAME);
         query.whereEqualTo(Business.Keys.CATEGORY, _category);
         query.countInBackground(new CountCallback() {
-            
+
             @Override
             public void done(int count, ParseException e) {
                 if (e != null) {
                     Log.e(TAG, "failed counting businesses with certain caregory");
                     return;
                 }
-                
-                if (view != null) {
-                    TextView numBusinessesTxt = (TextView) view
-                            .findViewById(R.id.customer_category_card_num_businesses_txt);
-                    
-                    if (numBusinessesTxt != null) {
-                        numBusinessesTxt.setText(String.valueOf(count) + (count == 1 ? " Business" : " Businesses"));
-                    }
+
+                if (numBusinessesTxt != null) {
+                    numBusinessesTxt.setText(String.valueOf(count)
+                            + (count == 1 ? " Business" : " Businesses"));
                 }
             }
         });
-        
-        
+    }
+
+    class CategoryCardThumbnail extends CardThumbnail {
+
+        public CategoryCardThumbnail(Context context) {
+            super(context);
+        }
+
+        @Override
+        public void setupInnerViewElements(ViewGroup parent, View viewImage) {
+            if (viewImage != null) {
+                if (parent != null && parent.getResources() != null) {
+                    DisplayMetrics metrics = parent.getResources().getDisplayMetrics();
+
+                    int base = 98;
+
+                    if (metrics != null) {
+                        viewImage.getLayoutParams().width = (int) (base * metrics.density);
+                        viewImage.getLayoutParams().height = (int) (base * metrics.density);
+                    } else {
+                        viewImage.getLayoutParams().width = 196;
+                        viewImage.getLayoutParams().height = 196;
+                    }
+                }
+            }
+
+        }
     }
 
 }
