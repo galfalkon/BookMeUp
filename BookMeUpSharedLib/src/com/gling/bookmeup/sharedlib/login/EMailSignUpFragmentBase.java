c7c92752d7ccc861d30f33e897f2a960bbda328c
galfalkon <galfalkon@hotmail.com>
2014-11-01 11:39:00 +0200

    Remove keyboard after signup

--- BookMeUpSharedLib/src/com/gling/bookmeup/sharedlib/login/EMailSignUpFragmentBase.java
+++ BookMeUpSharedLib/src/com/gling/bookmeup/sharedlib/login/EMailSignUpFragmentBase.java
@@ -5,11 +5,15 @@ import android.content.Context;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 
 import com.gling.bookmeup.main.FragmentsManagerUtils;
 import com.gling.bookmeup.main.OnClickListenerFragment;
@@ -24,12 +28,14 @@ import com.parse.SignUpCallback;
 import de.keyboardsurfer.android.widget.crouton.Crouton;
 import de.keyboardsurfer.android.widget.crouton.Style;
 
-public abstract class EMailSignUpFragmentBase extends OnClickListenerFragment {
-
+public abstract class EMailSignUpFragmentBase extends OnClickListenerFragment implements OnEditorActionListener 
+{
     private static final String TAG = "EMailSignUpFragmentBase";
 
-    private EditText edtUserName, edtEmail, edtPassword, edtPasswordVerification;
+    private EditText _edtUserName, _edtEmail, _edtPassword, _edtPasswordVerification;
 
+    protected abstract void createUserDataAfterSignup(ParseUser user);
+    
     public int getFragmentLayoutId() {
         return R.layout.login_email_signup_fragment;
     }
@@ -38,13 +44,16 @@ public abstract class EMailSignUpFragmentBase extends OnClickListenerFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = super.onCreateView(inflater, container, savedInstanceState);
 
-        edtEmail = (EditText) view.findViewById(R.id.email_signup_edtEmail);
-        edtPassword = (EditText) view.findViewById(R.id.email_signup_edtPassword);
-        edtPasswordVerification = (EditText) view.findViewById(R.id.email_signup_edtPasswordVerification);
-        edtUserName = (EditText) view.findViewById(R.id.email_signup_edtUserName);
+        _edtUserName = (EditText) view.findViewById(R.id.email_signup_edtUserName);
+        _edtEmail = (EditText) view.findViewById(R.id.email_signup_edtEmail);
+        _edtPassword = (EditText) view.findViewById(R.id.email_signup_edtPassword);
+        _edtPasswordVerification = (EditText) view.findViewById(R.id.email_signup_edtPasswordVerification);
+        
+        _edtPasswordVerification.setOnEditorActionListener(this);
 
         InputMethodManager imgr = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
-        imgr.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
+        imgr.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+        
         return view;
     }
 
@@ -54,21 +63,40 @@ public abstract class EMailSignUpFragmentBase extends OnClickListenerFragment {
 
         if (viewClickedId == R.id.email_signup_btnSignUp) 
         {
-			if (!validateInput()) 
-			{
-                return;
-            }
+        	handleSignupClick();
+        	return;
 		} else 
 		{
 			return;
 		}
-
-        final ProgressDialog progressDialog = ProgressDialog.show(getActivity(), null, "Signing up...");
+    }
+    
+    @Override
+	public boolean onEditorAction(TextView v, int actionId, KeyEvent event) 
+	{
+		switch (actionId)
+		{
+		case EditorInfo.IME_ACTION_DONE:
+			handleSignupClick();
+			return true;
+		default:
+			return false;
+		}
+	}
+    
+    private void handleSignupClick()
+    {
+    	if (!validateInput()) 
+		{
+            return;
+        }
+    	
+    	final ProgressDialog progressDialog = ProgressDialog.show(getActivity(), null, "Signing up...");
 
         final ParseUser user = new ParseUser();
-        user.setUsername(edtUserName.getText().toString());
-        user.setEmail(edtEmail.getText().toString());
-        user.setPassword(edtPassword.getText().toString());
+        user.setUsername(_edtUserName.getText().toString());
+        user.setEmail(_edtEmail.getText().toString());
+        user.setPassword(_edtPassword.getText().toString());
 
         user.signUpInBackground(new SignUpCallback() {
             @Override
@@ -97,61 +125,62 @@ public abstract class EMailSignUpFragmentBase extends OnClickListenerFragment {
 
                 createUserDataAfterSignup(user);
                 
+                InputMethodManager imgr = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                imgr.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+                
                 Crouton.showText(getActivity(),"Please verify your Email address", Style.CONFIRM);
                 FragmentsManagerUtils.goToNextFragment(getActivity(), R.id.login_container, ((LoginMainActivityBase)getActivity()).getEmailLoginFragmentInstance());
             }
         });
     }
     
-    protected abstract void createUserDataAfterSignup(ParseUser user);
-
     private boolean validateInput() {
 
         // Reset errors.
-        edtUserName.setError(null);
-        edtEmail.setError(null);
-        edtPassword.setError(null);
-        edtPasswordVerification.setError(null);
+        _edtUserName.setError(null);
+        _edtEmail.setError(null);
+        _edtPassword.setError(null);
+        _edtPasswordVerification.setError(null);
 
-        String userName = edtUserName.getText().toString();
-        String email = edtEmail.getText().toString();
-        String password = edtPassword.getText().toString();
-        String passwordVerification = edtPasswordVerification.getText().toString();
+        String userName = _edtUserName.getText().toString();
+        String email = _edtEmail.getText().toString();
+        String password = _edtPassword.getText().toString();
+        String passwordVerification = _edtPasswordVerification.getText().toString();
 
         boolean isValid = true;
         View focusView = null;
 
         if (TextUtils.isEmpty(password)) {
-            edtPassword.setError(getString(R.string.error_required_field));
-            focusView = edtPassword;
+            _edtPassword.setError(getString(R.string.error_required_field));
+            focusView = _edtPassword;
             isValid = false;
         } else if (password.length() < 4) {
-            edtPassword.setError("Passwords should be at least 4 characters long");
-            focusView = edtPassword;
+            _edtPassword.setError("Passwords should be at least 4 characters long");
+            focusView = _edtPassword;
             isValid = false;
         } else if (!TextUtils.equals(password, passwordVerification)) {
-            edtPasswordVerification.setError("Passwords don't match");
-            focusView = edtPasswordVerification;
+            _edtPasswordVerification.setError("Passwords don't match");
+            focusView = _edtPasswordVerification;
             isValid = false;
         }
         
         if (TextUtils.isEmpty(email)) {
-            edtEmail.setError(getString(R.string.error_required_field));
-            focusView = edtEmail;
+            _edtEmail.setError(getString(R.string.error_required_field));
+            focusView = _edtEmail;
             isValid = false;
         } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            edtEmail.setError("This email address is invalid");
-            focusView = edtEmail;
+            _edtEmail.setError("This email address is invalid");
+            focusView = _edtEmail;
             isValid = false;
         }
         
         if (TextUtils.isEmpty(userName)) {
-            edtUserName.setError(getString(R.string.error_required_field));
-            focusView = edtUserName;
+            _edtUserName.setError(getString(R.string.error_required_field));
+            focusView = _edtUserName;
             isValid = false;
         } else if (userName.length() < 4) {
-            edtUserName.setError("User name should be at least 4 characters long");
-            focusView = edtUserName;
+            _edtUserName.setError("User name should be at least 4 characters long");
+            focusView = _edtUserName;
             isValid = false;
         }
 
