0f3e16836eda33b8b2376b8d9527f4b4f81f6ef2
galfalkon <galfalkon@hotmail.com>
2014-11-05 00:09:43 +0200

    Customer bookings UI

--- BookMeUpForCustomer/src/com/gling/bookmeup/customer/CustomerMyBookingsFragment.java
+++ BookMeUpForCustomer/src/com/gling/bookmeup/customer/CustomerMyBookingsFragment.java
@@ -1,14 +1,14 @@
 package com.gling.bookmeup.customer;
 
 import it.gmariotti.cardslib.library.internal.Card;
-import it.gmariotti.cardslib.library.internal.Card.OnCardClickListener;
-import it.gmariotti.cardslib.library.internal.Card.OnLongCardClickListener;
 import it.gmariotti.cardslib.library.internal.CardExpand;
 import it.gmariotti.cardslib.library.internal.CardHeader;
 
 import java.util.Date;
 import java.util.List;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.ActionMode;
@@ -17,6 +17,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.gling.bookmeup.main.Constants;
 import com.gling.bookmeup.main.GenericMultiChoiceCardArrayAdapter;
@@ -132,60 +133,7 @@ public class CustomerMyBookingsFragment extends OnClickListenerFragment {
 		@Override
 		public Card generateCard(Booking booking) 
 		{
-			CardHeader cardHeader = new CardHeader(getActivity());
-			cardHeader.setTitle(booking.getBusiness().getName());
-			cardHeader.setButtonExpandVisible(true);
-	    	
-			CardExpand expand = new CardExpand(getActivity());
-			expand.setTitle("Last updated: " + Constants.DATE_TIME_FORMAT.format(booking.getUpdatedAt()));
-			
-			String cardTitle = 
-					"Service: " + booking.getServiceName() + "\n" +
-							"Date: " + Constants.DATE_TIME_FORMAT.format(booking.getDate()) + "\n";
-			String status;
-			int statusColor;
-			switch (booking.getStatus())
-			{
-			case Booking.Status.PENDING:
-				status = getString(R.string.customer_my_bookings_booking_pending_for_approval);
-				statusColor = getResources().getColor(android.R.color.holo_purple);
-				break;
-			case Booking.Status.APPROVED:
-				status = getString(R.string.customer_my_bookings_booking_approved);
-				statusColor = getResources().getColor(android.R.color.holo_green_light);
-				break;
-			case Booking.Status.CANCELED:
-			default:
-				status = getString(R.string.customer_my_bookings_booking_canceled);
-				statusColor = getResources().getColor(android.R.color.holo_red_light);
-				break;
-			}
-	    	Card card = new BookingCard(getActivity(), cardTitle, status, statusColor); 
-	    	card.addCardHeader(cardHeader);
-	    	card.addCardExpand(expand);
-			card.setTitle(cardTitle);
-			card.setOnClickListener(new OnCardClickListener() 
-			{
-				@Override
-				public void onClick(Card card, View view) 
-				{
-					card.doToogleExpand();
-				}
-			});
-			
-			if (booking.getStatus() != Booking.Status.CANCELED)
-			{
-				card.setOnLongClickListener(new OnLongCardClickListener() 
-				{
-					@Override
-					public boolean onLongClick(Card card, View view) 
-					{
-						return _bookingsCardAdapter.startActionMode(getActivity());
-					}
-				});
-			}
-
-	    	return card;
+			return new BookingCard(getActivity(), booking);
 		}
 	}
 	
@@ -210,4 +158,82 @@ public class CustomerMyBookingsFragment extends OnClickListenerFragment {
         	return false;
 		}
 	}
+	
+	private class BookingCard extends Card
+    {
+		private final Context _context;
+    	private final Booking _booking; 
+    	
+		public BookingCard(final Context context, Booking booking) 
+		{
+			super(context, R.layout.customer_my_bookings_booking_card);
+			
+			_context = context;
+			_booking = booking;
+			
+			CardHeader cardHeader = new CardHeader(context);
+			cardHeader.setTitle(booking.getBusiness().getName());
+			cardHeader.setButtonExpandVisible(true);
+	    	
+			CardExpand expand = new CardExpand(context);
+			expand.setTitle("Last updated: " + Constants.DATE_TIME_FORMAT.format(booking.getUpdatedAt()));
+			
+	    	setTitle(_booking.getBusiness().getName());
+	    	addCardHeader(cardHeader);
+	    	addCardExpand(expand);
+			setOnClickListener(new OnCardClickListener() 
+			{
+				@Override
+				public void onClick(Card card, View view) 
+				{
+					doToogleExpand();
+				}
+			});
+			
+			if (_booking.getStatus() != Booking.Status.CANCELED)
+			{
+				setOnLongClickListener(new OnLongCardClickListener() 
+				{
+					@Override
+					public boolean onLongClick(Card card, View view) 
+					{
+						return _bookingsCardAdapter.startActionMode(getActivity());
+					}
+				});
+			}
+		}
+		
+		@Override
+		public void setupInnerViewElements(ViewGroup parent, View view) 
+		{
+			TextView txtService = (TextView) view.findViewById(R.id.customer_my_bookings_booking_card_service);
+			txtService.setText(_booking.getServiceName());
+			
+			TextView txtDate = (TextView) view.findViewById(R.id.customer_my_bookings_booking_card_date);
+			txtDate.setText(Constants.DATE_TIME_FORMAT.format(_booking.getDate()));
+			
+			TextView txtStatus = (TextView) view.findViewById(R.id.customer_my_bookings_booking_card_status);
+			Resources resources = _context.getApplicationContext().getResources();
+			int statusStringResourceId, statusColorResourceId;
+			switch (_booking.getStatus())
+			{
+			case Booking.Status.PENDING:
+				statusStringResourceId = R.string.customer_my_bookings_booking_pending_for_approval;
+				statusColorResourceId = android.R.color.holo_purple;
+				break;
+			case Booking.Status.APPROVED:
+				statusStringResourceId = R.string.customer_my_bookings_booking_approved;
+				statusColorResourceId = android.R.color.holo_green_light;
+				break;
+			case Booking.Status.CANCELED:
+			default:
+				statusStringResourceId = R.string.customer_my_bookings_booking_canceled;
+				statusColorResourceId = android.R.color.holo_red_light;
+				break;
+			}
+			
+			txtStatus.setText(resources.getString(statusStringResourceId));
+			txtStatus.setTextColor(resources.getColor(statusColorResourceId));
+		}
+    }
 }
